04bbc1610d7c0cab04fc776cfe556a771dc224cc
jeeraponnnnnnnn <lps>
2021-10-16 17:59:47 +0700

    🔥 Remove code addInvestor(), removeInvestor(), containsInvestor() and add pool limit per user

--- contracts/SmartChefFoundingInvestor.sol
+++ contracts/SmartChefFoundingInvestor.sol
@@ -17,6 +17,9 @@ contract SmartChefFoundingInvestor is Ownable, ReentrancyGuard {
 
     EnumerableSet.AddressSet private _investors;
 
+    // Whether a limit is set for users
+    bool public hasUserLimit;
+
     // Accrued token per share
     uint256 public accTokenPerShare;
 
@@ -28,6 +31,9 @@ contract SmartChefFoundingInvestor is Ownable, ReentrancyGuard {
 
     // The block number of the last pool update
     uint256 public lastRewardBlock;
+
+    // The pool limit (0 if none)
+    uint256 public poolLimitPerUser;
 
     // PLEARN tokens created per block.
     uint256 public rewardPerBlock;
@@ -50,7 +56,7 @@ contract SmartChefFoundingInvestor is Ownable, ReentrancyGuard {
         uint256 initialAmount; // How many staked tokens the user has provided (just deposit)
         uint256 amount; // How many staked tokens the user has provided
         uint256 rewardDebt; // Reward debt
-        uint256 releasePerBlock; // unlocked token per block
+        uint256 unlockedPerBlock; // unlocked token per block
     }
 
     event AdminTokenRecovery(address tokenRecovered, uint256 amount);
@@ -58,6 +64,7 @@ contract SmartChefFoundingInvestor is Ownable, ReentrancyGuard {
     event DepositToInvestor(address indexed user, uint256 amount);
     event NewStartAndEndBlocks(uint256 startBlock, uint256 endBlock);
     event NewRewardPerBlock(uint256 rewardPerBlock);
+    event NewPoolLimit(uint256 poolLimitPerUser);
     event RewardsStop(uint256 blockNumber);
     event Withdraw(address indexed user, uint256 amount);
 
@@ -68,6 +75,7 @@ contract SmartChefFoundingInvestor is Ownable, ReentrancyGuard {
      * @param _rewardPerBlock: reward per block (in rewardToken)
      * @param _startBlock: start block
      * @param _bonusEndBlock: end block
+     * @param _poolLimitPerUser: pool limit per user in stakedToken (if any, else 0)
      */
     constructor(
         IBEP20 _stakedToken,
@@ -75,7 +83,8 @@ contract SmartChefFoundingInvestor is Ownable, ReentrancyGuard {
         SmartChefFoundingInvestorTreasury _treasury,
         uint256 _rewardPerBlock,
         uint256 _startBlock,
-        uint256 _bonusEndBlock
+        uint256 _bonusEndBlock,
+        uint256 _poolLimitPerUser
     ) {
         stakedToken = _stakedToken;
         rewardToken = _rewardToken;
@@ -83,6 +92,11 @@ contract SmartChefFoundingInvestor is Ownable, ReentrancyGuard {
         rewardPerBlock = _rewardPerBlock;
         startBlock = _startBlock;
         bonusEndBlock = _bonusEndBlock;
+
+        if (_poolLimitPerUser > 0) {
+            hasUserLimit = true;
+            poolLimitPerUser = _poolLimitPerUser;
+        }
 
         uint256 decimalsRewardToken = uint256(rewardToken.decimals());
         require(decimalsRewardToken < 30, "Must be inferior to 30");
@@ -97,7 +111,8 @@ contract SmartChefFoundingInvestor is Ownable, ReentrancyGuard {
      * @notice Collect reward tokens (if any)
      * @param _amount: 0
      */
-    function deposit(uint256 _amount) external nonReentrant onlyInvestor {
+    function deposit(uint256 _amount) external nonReentrant {
+        require(_amount == 0, "Cannot be staked token");
         UserInfo storage user = userInfo[msg.sender];
 
         _updatePool();
@@ -119,9 +134,11 @@ contract SmartChefFoundingInvestor is Ownable, ReentrancyGuard {
      * @param _amount: amount to deposit (in stakedTokens)
      */
     function depositToInvestor(uint256 _amount, address _address) public onlyOwner {
-        require(isInvestor(_address), "Investor: wut?");
-        
         UserInfo storage user = userInfo[_address];
+
+        if (hasUserLimit) {
+            require(_amount.add(user.amount) <= poolLimitPerUser, "User amount above limit");
+        }
 
         _updatePool();
 
@@ -135,8 +152,9 @@ contract SmartChefFoundingInvestor is Ownable, ReentrancyGuard {
         if (_amount > 0) {
             user.amount = user.amount.add(_amount);
             user.initialAmount = user.amount;
-            user.releasePerBlock = user.initialAmount / (bonusEndBlock - startBlock);
+            user.unlockedPerBlock = user.initialAmount / (bonusEndBlock - startBlock);
             stakedToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+            EnumerableSet.add(_investors, _address);
         }
 
         user.rewardDebt = user.amount.mul(accTokenPerShare).div(PRECISION_FACTOR);
@@ -148,7 +166,7 @@ contract SmartChefFoundingInvestor is Ownable, ReentrancyGuard {
      * @notice Withdraw staked tokens and collect reward tokens
      * @param _amount: amount to withdraw (in rewardToken)
      */
-    function withdraw(uint256 _amount) external nonReentrant onlyInvestor {
+    function withdraw(uint256 _amount) external nonReentrant {
         UserInfo storage user = userInfo[msg.sender];
         require(user.amount >= _amount, "Amount to withdraw too high");
 
@@ -158,7 +176,7 @@ contract SmartChefFoundingInvestor is Ownable, ReentrancyGuard {
 
         if (_amount > 0) {
             uint256 multiplier = _getMultiplier(startBlock, block.number);
-            uint256 unlockedToken = multiplier.mul(user.releasePerBlock);
+            uint256 unlockedToken = multiplier.mul(user.unlockedPerBlock);
             uint256 pendingUnlocked = unlockedToken.sub(user.initialAmount.sub(user.amount));
 
             require(pendingUnlocked  >= _amount, "Amount to withdraw too high");
@@ -179,19 +197,19 @@ contract SmartChefFoundingInvestor is Ownable, ReentrancyGuard {
      * @notice Withdraw staked tokens without caring about rewards rewards
      * @dev Only callable by owner. Needs to be for emergency.
      */
-    function emergencyWithdraw(address _from) external onlyOwner {
+    function emergencyWithdraw(address _from) external onlyOwner nonReentrant {
         require(block.number < startBlock, "Pool has started");
-        require(isInvestor(_from), "Investor: wut?");
 
         UserInfo storage user = userInfo[_from];
         uint256 amountToTransfer = user.amount;
         user.initialAmount = 0;
         user.amount = 0;
         user.rewardDebt = 0;
-        user.releasePerBlock = 0;
+        user.unlockedPerBlock = 0;
 
         if (amountToTransfer > 0) {
             stakedToken.safeTransfer(address(msg.sender), amountToTransfer);
+            EnumerableSet.remove(_investors, _from);
         }
     }
 
@@ -216,6 +234,24 @@ contract SmartChefFoundingInvestor is Ownable, ReentrancyGuard {
      */
     function stopReward() external onlyOwner {
         bonusEndBlock = block.number;
+    }
+
+    /*
+     * @notice Update pool limit per user
+     * @dev Only callable by owner.
+     * @param _hasUserLimit: whether the limit remains forced
+     * @param _poolLimitPerUser: new pool limit per user
+     */
+    function updatePoolLimitPerUser(bool _hasUserLimit, uint256 _poolLimitPerUser) external onlyOwner {
+        require(hasUserLimit, "Must be set");
+        if (_hasUserLimit) {
+            require(_poolLimitPerUser > poolLimitPerUser, "New limit must be higher");
+            poolLimitPerUser = _poolLimitPerUser;
+        } else {
+            hasUserLimit = _hasUserLimit;
+            poolLimitPerUser = 0;
+        }
+        emit NewPoolLimit(poolLimitPerUser);
     }
 
     /*
@@ -277,7 +313,7 @@ contract SmartChefFoundingInvestor is Ownable, ReentrancyGuard {
         UserInfo storage user = userInfo[_user];
         if (block.number > startBlock && user.amount > 0) {
             uint256 multiplier = _getMultiplier(startBlock, block.number);
-            uint256 unlockedToken = multiplier.mul(user.releasePerBlock);
+            uint256 unlockedToken = multiplier.mul(user.unlockedPerBlock);
             return unlockedToken.sub(user.initialAmount.sub(user.amount));
         } else {
             return 0;
@@ -330,33 +366,13 @@ contract SmartChefFoundingInvestor is Ownable, ReentrancyGuard {
         treasury.safeRewardTransfer(_to, _amount);
     }
 
-    function addInvestor(address _addInvestor) public onlyOwner returns (bool) {
-        require(_addInvestor != address(0), "SmartChefFoundingInvestor: _addInvestor is the zero address");
-        return EnumerableSet.add(_investors, _addInvestor);
-    }
-
-    function delInvestor(address _delInvestor) public onlyOwner returns (bool) {
-        require(_delInvestor != address(0), "SmartChefFoundingInvestor: _delInvestor is the zero address");
-        return EnumerableSet.remove(_investors, _delInvestor);
-    }
-
     function getInvestorLength() public view returns (uint256) {
         return EnumerableSet.length(_investors);
-    }
-
-    function isInvestor(address account) public view returns (bool) {
-        return EnumerableSet.contains(_investors, account);
     }
 
     function getInvestor(uint256 _index) public view onlyOwner returns (address){
         require(_index <= getInvestorLength() - 1, "SmartChefFoundingInvestor: index out of bounds");
         return EnumerableSet.at(_investors, _index);
-    }
-
-    // modifier for investor function
-    modifier onlyInvestor() {
-        require(isInvestor(msg.sender) || msg.sender == owner(), "caller is not the investor");
-        _;
     }
 
 }
